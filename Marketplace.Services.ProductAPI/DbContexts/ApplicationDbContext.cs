e6b41dfbfa799f338c4585a3c172441166baeb38
Akloni1 <61523151+Akloni1@users.noreply.github.com>
2024-04-05 17:03:44 +0300

    main-02-006: Добавили миграцию и таблицу в бд.

---
+++ Marketplace.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Marketplace.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Marketplace.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    if (!optionsBuilder.IsConfigured)
+        //    {
+        //        optionsBuilder.UseSqlServer("Server=DESKTOP-JUI9V07;Database=Competitions;Trusted_Connection=True;");
+        //    }
+        //}
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
